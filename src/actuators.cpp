e457f81caee3e026a11374408ddb255afb4483a0
cdeshpande706-wq <cdeshpande706@gmail.com>
2026-08-22 21:57:33 +0530

    Update actuators.cpp

--- src/actuators.cpp
+++ src/actuators.cpp
@@ -1,1 +1,16 @@
+#include "actuators.h"
 
+#include <ESP32Servo.h>
+
+Servo servo;
+
+const int SERVO_PIN = 13;
+
+void initActuators() {
+  servo.attach(SERVO_PIN);
+  servo.write(90);
+}
+
+void setServoPosition(int position) {
+  servo.write(position);
+}
