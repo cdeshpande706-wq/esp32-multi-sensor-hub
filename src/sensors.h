1da9897f29d2124b7a49d8b39c128c505c76e8a2
cdeshpande706-wq <cdeshpande706@gmail.com>
2026-08-22 21:53:42 +0530

    Update sensors.h

--- src/sensors.h
+++ src/sensors.h
@@ -1,1 +1,14 @@
+#ifndef SENSORS_H
+#define SENSORS_H
 
+struct IMUData {
+  float accelX;
+  float accelY;
+  float accelZ;
+};
+
+void initSensors();
+IMUData readIMU();
+float readDistance();
+
+#endif
