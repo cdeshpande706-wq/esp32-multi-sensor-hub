cc4686ee19e3dc989615a0f75a7eada22be8dbd8
cdeshpande706-wq <cdeshpande706@gmail.com>
2026-08-22 21:54:44 +0530

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,52 @@
+#include <Arduino.h>
 
+#include "sensors.h"
+#include "actuators.h"
+
+void setup() {
+  Serial.begin(115200);
+
+  initSensors();
+  initActuators();
+
+  Serial.println("ESP32 Multi-Sensor Hub started!");
+}
+
+void loop() {
+
+  IMUData imu = readIMU();
+  float distance = readDistance();
+
+  int servoPosition;
+
+  if (distance < 100) {
+    servoPosition = 30;
+  } else {
+    servoPosition = 90;
+  }
+
+  setServoPosition(servoPosition);
+
+  Serial.println("----------------------------");
+
+  Serial.print("Accel X: ");
+  Serial.print(imu.accelX);
+
+  Serial.print(" | Y: ");
+  Serial.print(imu.accelY);
+
+  Serial.print(" | Z: ");
+  Serial.println(imu.accelZ);
+
+  Serial.print("Distance: ");
+  Serial.print(distance);
+
+  Serial.println(" cm");
+
+  Serial.print("Servo Position: ");
+  Serial.print(servoPosition);
+
+  Serial.println("°");
+
+  delay(500);
+}
